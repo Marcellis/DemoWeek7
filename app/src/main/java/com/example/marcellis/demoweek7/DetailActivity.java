9f0b92c33dbb1ee5a62c434c362e7bf63ef44ae2
Marcellis <m.m.c.m.marcellis@hva.nl>
2016-10-13 13:41:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/marcellis/demoweek7/DetailActivity.java
@@ -0,0 +1,15 @@
+package com.example.marcellis.demoweek7;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+public class DetailActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_detail);
+
+    }
+}
